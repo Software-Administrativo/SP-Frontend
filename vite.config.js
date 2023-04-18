305f2785540a3254bc807a097c468fd149db9776
Laura Susano <lauritasusano.10@gmail.com>
2023-04-18 18:43:06 -0500

    feat(quasar): setup quasar framework

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 // Creación de alias
 import { fileURLToPath, URL } from "node:url";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
   // GitHub Pages
   base: "/SP-frontend/",
   resolve: {
-    // Aquí se pueden añadir los alias a las carpetas
+    // Alias de las carpetas
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    quasar({
+      sassVariables: "src/quasar-variables.sass",
+    }),
+  ],
 });
