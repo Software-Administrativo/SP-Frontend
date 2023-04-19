5d765d81da18c99d1f88fefe757a5ed8ff3b7640
J6-Lovelace <lauritasusano.10@gmail.com>
2023-04-18 21:36:53 -0500

    Updates

--- assets/AboutView-7d6285a3.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-611d07fd.js";const t={};function n(r,s){return c(),o("span",null,"About view")}const _=e(t,[["render",n]]);export{_ as default};
---
+++ assets/CostView-de52ebc9.js
@@ -0,0 +1,1 @@
+import{_ as e,a as c,c as t}from"./index-74b1b283.js";const n={};function o(s,r){return c(),t("span",null,"Cost View")}const _=e(n,[["render",o]]);export{_ as default};
