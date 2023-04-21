f54d8fd4a8163c7cedc5760c618ca25b0d0b4114
JuanF3r <124316077+JuanF3r@users.noreply.github.com>
2023-04-21 09:18:01 -0500

    feat(sign-in): update image, restructure form

--- src/modules/sign-in/Form.vue
+++ src/modules/sign-in/Form.vue
@@ -1,38 +1,48 @@
 <template>
-  <div class="container row">
-    <div class="col-12 row">
-      <div class="col-12 row">
-        <p class="select-type-document col-3">Tipo</p>
-        <p class="col-8">Número de Documento</p>
-      </div>
+  <div class="row">
+    <div class="col-3">
+      <label for="type">Tipo</label>
       <q-select
-        class="select-type-document col-3 q-pa-none"
+        class="select-type-document q-pa-none"
+        dense
         filled
+        name="type"
         v-model="typeDocument"
         :options="types"
       />
-      <q-input class="col-8" filled v-model="documentUser" type="number" />
     </div>
-    <div class="col-12 row q-mt-md">
-    <div class="col-12 row"> <p>Contraseña</p></div>
+    <div class="col">
+      <label for="document">Número de documento</label>
       <q-input
-        class="col-12"
-        v-model="password"
+        class=""
+        dense
         filled
-        :type="isPwd ? 'password' : 'text'"
-        hint="Contraseña incorrecta"
-      >
-        <template v-slot:append>
-          <q-icon
-            :name="isPwd ? 'visibility_off' : 'visibility'"
-            class="cursor-pointer"
-            @click="isPwd = !isPwd"
-          />
-        </template>
-      </q-input>
+        name="document"
+        v-model="documentUser"
+        type="number"
+      />
     </div>
-    <q-btn class="btn-login text-white full-width q-mt-lg">Entrar</q-btn>
   </div>
+  <div class="row q-mt-md">
+    <label>Contraseña</label>
+    <q-input
+      class="col-12"
+      v-model="password"
+      dense
+      filled
+      :type="isPwd ? 'password' : 'text'"
+      hint="Contraseña incorrecta"
+    >
+      <template v-slot:append>
+        <q-icon
+          :name="isPwd ? 'visibility_off' : 'visibility'"
+          class="cursor-pointer"
+          @click="isPwd = !isPwd"
+        />
+      </template>
+    </q-input>
+  </div>
+  <q-btn class="btn-login text-white full-width q-mt-lg">Entrar</q-btn>
 </template>
 
 <script setup>
@@ -53,10 +63,4 @@ const types = ["CC", "CE", "NIT", "NIP", "NUIP", "PA"];
 .btn-login {
   background-color: #0068a5;
 }
-
-.container {
-  width: 470px;
-}
-
-
 </style>
--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="bg-login col-12 row">
-    <div class="col-6 bg-white">
+    <div class="col-6 bg-white container">
       <div class="si-logo col-12 row q-mt-md q-ml-md"></div>
-        <div class="form col-12">
-          <h2 class="q-mb-md q-mr-xl">Bienvenido</h2>
-          <h5 class="col-4 text-grey q-mt-xs q-mb-md q-mr-xl">Ingresa a tu cuenta</h5>
-          <Form />
-        </div>
+      <div class="form">
+        <h2 class="q-mb-md">Bienvenido</h2>
+        <h5 class="text-grey q-mt-xs q-mb-md">Ingresa a tu cuenta</h5>
+        <Form />
+      </div>
     </div>
   </div>
 </template>
@@ -17,7 +17,7 @@ import Form from "@/modules/sign-in/Form.vue";
 
 <style scoped>
 .bg-login {
-  background: url(../assets/sign-in/image-background.png); 
+  background: url(../assets/sign-in/image-background.png);
   background-position: center center;
   background-size: cover;
   background-repeat: no-repeat;
@@ -29,16 +29,22 @@ import Form from "@/modules/sign-in/Form.vue";
   background-image: url(../assets/sign-in/sign-in-logo.png);
   background-repeat: no-repeat;
   background-size: 28%;
+  max-width: 600px;
+  min-width: 300px;
   height: 20%;
 }
-.form{
+.form {
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  text-align: left;
-  margin-left: 22%;
+  width: 60%;
+  max-width: 380px;
+  min-width: 300px;
+  margin: 0 auto;
 }
 
-
+@media (max-width: 700px){
+  .container{
+    width: 100vw;
+  }
+}
 </style>
