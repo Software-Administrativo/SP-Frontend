fa0ac8e885f1e1dea1eaff66168f8ac70e9c00ff
Laura Susano <lauritasusano.10@gmail.com>
2023-05-03 16:41:39 -0500

    fix: solve conflicts with dev branch

--- src/api/maintenance/people/index.js
+++ src/api/maintenance/people/index.js
@@ -1,5 +1,5 @@
+import { getToken } from '@/helpers';
 import { sugarAxios } from "../../global";
-import { getToken } from '@/helpers'
 
 /**
  * data: data people
@@ -31,16 +31,12 @@ const postPeople = async (type) => {
       birthDate: type.birthDate,
       phone: type.phone,
       bloodType: type.bloodType,
-<<<<<<< HEAD
       peopleType: type.peopleType
     },{
       headers: {
         token:
           tokenExist,
       },
-=======
-      peopleType: type.peopleType,
->>>>>>> c578c1e4546632a48fe4643d6f866ce7f05e47eb
     });
     return data;
   } catch (error) {
