b09a9184a81f553aa1eedfa251720f528e488bd6
JuanF3r <juanfercdc@gmail.com>
2023-05-29 15:35:22 -0500

    Updates

--- assets/OrderView-a14c31dd.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as r,c}from"./index-9b0f9ebe.js";const n={};function o(t,s){return r(),c("span",null,"Order View")}const _=e(n,[["render",o]]);export{_ as default};
--- assets/TransformationView-1c94ad1c.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as n,c as o}from"./index-9b0f9ebe.js";const r={};function a(c,t){return n(),o("span",null,"Transformation View")}const _=e(r,[["render",a]]);export{_ as default};
