80ffa9f5a966b221fa6b2a6f59b573522c39cb55
JuanF3r <juanfercdc@gmail.com>
2023-06-08 16:08:51 -0500

    feat(pages): update forgot-password 'file NewPassword'

--- src/pages/forgot-password/NewPassword.vue
+++ src/pages/forgot-password/NewPassword.vue
@@ -12,73 +12,51 @@
         <div class="form column">
           <h3 class="q-mb-md">Recupera tu contraseña</h3>
           <h5 class="text-grey q-mt-xs q-mb-md">Ingresa tu nueva contraseña</h5>
-          <!-- <Input
-            class="input q-mb-md"
-            label="Contraseña"
-            type="text"
-            :ruless="rules"
-            :required="true"
+          <span>Contraseña <label class="text-red">*</label></span>
+          <q-input
+            dense
+            filled
+            :type="isPassword ? 'password' : 'text'"
+            error-message="Las contraseñas no coinciden"
+            :error="!isValid"
             v-model="password"
-            @onWrite="getInputPassword"
-            
-          />
-          <Input
-            class="input q-mb-md"
-            label="Verifica la contraseña"
-            type="text"
-            :ruless="rules"
-            :required="true"
+            @keyup="validatePasswords"
+          >
+            <template v-slot:append>
+              <q-icon
+                :name="isPassword ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPassword = !isPassword"
+              />
+            </template>
+          </q-input>
+          <span>Confirmar contraseña <label class="text-red">*</label></span>
+          <q-input
+            dense
+            filled
+            :type="isPassword ? 'password' : 'text'"
+            error-message="Las contraseñas no coinciden"
+            :error="!isValid"
             v-model="confirmPassword"
-            @onWrite="getInputConfirmPassword"
-          /> -->
-
-          <q-input 
-          v-model="password" 
-          filled 
-          :type="isPwd ? 'password' : 'text'" 
-          hint="Password with toggle"
-          error-message="Las contraseñas no coinciden"
-          :error="!isValid"
-          @keyup="validatePasswords"
+            @keyup="validatePasswords"
           >
-          <template v-slot:append>
-          <q-icon
-            :name="isPwd ? 'visibility_off' : 'visibility'"
-            class="cursor-pointer"
-            @click="isPwd = !isPwd"
-          />
-        </template>
-      </q-input>
-
-          <q-input 
-          v-model="confirmPassword" 
-          filled 
-          :type="isPwd ? 'password' : 'text'" 
-          hint="Password with toggle"
-          error-message="Las contraseñas no coinciden"
-          :error="!isValid"
-          @keyup="validatePasswords"
-          >
-        <template v-slot:append>
-          <q-icon
-            :name="isPwd ? 'visibility_off' : 'visibility'"
-            class="cursor-pointer"
-            @click="isPwd = !isPwd"
-          />
-        </template>
-      </q-input>
-
+            <template v-slot:append>
+              <q-icon
+                :name="isPassword ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPassword = !isPassword"
+              />
+            </template>
+          </q-input>
           <template v-if="isInactive">
-            <q-btn 
-            disable 
-            class="btn-login text-white full-width q-mt-lg"
+            <q-btn disable class="btn-login text-white full-width q-mt-none"
               >Enviar</q-btn
             >
           </template>
           <template v-else>
-            <q-btn 
-            @click="postDataPassword"
-            class="btn-login text-white full-width q-mt-lg"
+            <q-btn
+              @click="postDataPassword"
+              class="btn-login text-white full-width q-mt-none"
               >Enviar</q-btn
             >
           </template>
@@ -94,10 +72,10 @@
 
 <script setup>
 import { postChangePassword } from "@/api/system";
-import { ref, computed,onBeforeMount } from "vue";
-import Input from "@/commons/forms/Input.vue";
-import { useQuasar } from "quasar"
+import { ref, computed, onBeforeMount } from "vue";
+import { useQuasar } from "quasar";
 import { useRouter } from "vue-router";
+import { RESPONSES } from "@/helpers";
 
 const props = defineProps({
   error: {
@@ -107,84 +85,83 @@ const props = defineProps({
   },
 });
 
-const isPwd = ref(true)
+const isPassword = ref(true);
 
 const $q = useQuasar();
 const $router = useRouter();
 const password = ref("");
 const confirmPassword = ref("");
-let token = null
+let token = null;
 
-const isValid = ref(true)
-
-const rules = [
-  (v) => !!v || "Este campo es requerido",
-  () => passwordMatch.value || "Las contraseñas no coinciden",
-];
-
-const passwordMatch = computed(() => {
-    return password.value === confirmPassword.value;
-  });
+const isValid = ref(true);
 
 const validatePasswords = () => {
-  if(password.value != confirmPassword.value) isValid.value = false
-  else isValid.value = true
+  if (password.value != confirmPassword.value) isValid.value = false;
+  else isValid.value = true;
 };
 
 const isInactive = computed(() => {
-  return (password.value === "" || confirmPassword.value === "")
+  return (
+    password.value === "" ||
+    confirmPassword.value === "" ||
+    password.value != confirmPassword.value
+  );
 });
-
 
 onBeforeMount(() => {
   //validar si en la ruta viene un id
-  token = $router.currentRoute.value.params.token
-  if(token == null || token == ""){
-    $router.replace('/')
+  token = $router.currentRoute.value.params.token;
+  if (token == null || token == "") {
+    $router.replace("/");
   }
 });
 
 const isLoading = ref(false);
 
-const getInputPassword = (value) => {
-  password.value = value;
+const showNotification = (type, message) => {
+  $q.notify({
+    type: type,
+    message: message,
+    position: "top",
+  });
 };
 
-const getInputConfirmPassword = (value) => {
-  confirmPassword.value = value;
-}
-const showNotification = (type, message) => {
-    $q.notify({
-      type: type,
-      message: message,
-      position: "top",
+async function postDataPassword() {
+  isLoading.value = true;
+  try {
+    const data = await postChangePassword({
+      token,
+      password: password.value,
     });
-  };
+    isLoading.value = false;
+    
+    let response = data?.response?.data?.errors[0]?.msg;
 
-  async function postDataPassword() {
-    isLoading.value = true;
-    try {
-      await postChangePassword({
-        token,
-        password: password.value,
-      });
-      isLoading.value = false;
-      showNotification("positive", "Contraseña cambiada con exito");
-    } catch {
-      isLoading.value = false;
+    if (response == RESPONSES.LENGTHPASSWORD) {
       showNotification(
         "negative",
-        "Ocurrió un error, por favor verifique los datos"
+        "La contraseña debe tener de 6 a 20 carácteres"
       );
-    }
-  };
-
-
+    } else if (response == RESPONSES.RULESPASSWORD) {
+      showNotification(
+        "negative",
+        "La contraseña debe tener una letra mayúscula, una letra minúscula y un número"
+      );
+    } else {
+    showNotification("positive", "Contraseña cambiada con exito");
+    setTimeout(() => {
+      $router.push({ name: "sign-in"});
+    }, 2000);}
+  } catch {
+    isLoading.value = false;
+    showNotification(
+      "negative",
+      "Ocurrió un error, por favor verifique los datos"
+    );
+  }
+}
 </script>
 <style scoped>
-.input{
-  width: 50%;
-}
 .background-sugar {
   background: url(../../assets/sign-in/image-background.png);
   background-position: center center;
